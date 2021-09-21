bd735cb43b26ed0488b9485e4152f641584fca9a
joaoavsantos <42188271+joaoavsantos@users.noreply.github.com>
2021-09-21 10:59:49 +0100

    Update getvin.js

--- www/getvin.js
+++ www/getvin.js
@@ -1,6 +1,6 @@
 var exec = require('cordova/exec');
 
-var PLUGIN_NAME = 'getvincordovaplugin';
+var PLUGIN_NAME = 'GetVinPlugin';
 
 exports.getVin = function (success, error) {
   exec(success, error, PLUGIN_NAME, "getVin", []);
